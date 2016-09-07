e70999d103b21db297444dfe3215436c071f487d
Taylor Ham <mynameisham@gmail.com>
2016-09-06 17:35:57 -0700

    working example

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,22 +1,74 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import TodoList from './TodoList'
+import update from 'react-addons-update'
+import { TodoList } from './TodoList'
 import { Sidebar } from './Sidebar'
 
 class Main extends React.Component {
   state = {
-    isAlertVisible: false,
+    activeTags: [],
     todos: [
-      {id: 1, title: 'Download react-starter-es6', completed: true},
-      {id: 2, title: 'Read the docs', completed: true},
-      {id: 3, title: 'Make a component', completed: false},
-      {id: 4, title: 'Combine components to make an app', completed: false},
-      {id: 5, title: 'Build a social network empire', completed: false}
+      {title: 'Download react-starter-es6', completed: true, tags: ['react']},
+      {title: 'Read the docs', completed: true, tags: ['docs']},
+      {title: 'Make a component', completed: false, tags: ['react', 'html', 'js']},
+      {title: 'Combine components to make an app', completed: false, tags: ['react', 'html', 'js']},
+      {title: 'Build a social network empire', completed: false, tags: ['facebook']}
     ]
   }
 
-  handleToggleAlert = (id, message) => {
-    this.setState({isAlertVisible: !this.state.isAlertVisible})
+  handleToggleCompleted = (idx, e) => {
+    e.preventDefault()
+
+    const updatedTodos = update(this.state.todos, {[idx]: {completed: { $set: !this.state.todos[idx].completed }}})
+    this.setState({todos: updatedTodos})
+  }
+
+  handleAddItem = () => {
+    const title = document.getElementById('addItemField').value
+    const originalTags = document.getElementById('addTagsField').value.split(', ')
+
+    if (!title) {
+      return alert('Title cannot be blank')
+    }
+
+    const removeDuplicateTags = new Set()
+    originalTags.map(tag => removeDuplicateTags.add(tag))
+    const noDuplicateTags = [...removeDuplicateTags]
+
+    const newTodoItem = {title: title, completed: false, tags: noDuplicateTags}
+    const updatedTodos = update(this.state.todos, { $push: [newTodoItem] })
+
+    document.getElementById('addItemField').value = ''
+    document.getElementById('addTagsField').value = ''
+
+    this.setState({todos: updatedTodos})
+  }
+
+  handleDeleteItem = (idx) => {
+    const updatedTodos = update(this.state.todos, { $splice: [[idx, 1]] })
+
+    this.setState({todos: updatedTodos })
+  }
+
+  handleTagFilter = (tag, select = true) => {
+    const updatedTags = update(this.state.activeTags, {})
+    const activeSet = new Set()
+
+    this.state.activeTags.map(tag => activeSet.add(tag))
+
+    if (select) {
+      if (activeSet.has(tag)) {
+        const index = updatedTags.indexOf(tag)
+        updatedTags.splice(index, 1)
+      } else {
+        updatedTags.push(tag)
+      }
+    } else {
+      const index = updatedTags.indexOf(tag)
+      updatedTags.splice(index, 1)
+    }
+
+    this.setState({activeTags: updatedTags})
   }
 
   render() {
@@ -24,10 +76,20 @@ class Main extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-md-8">
-            <TodoList />
+            <TodoList
+              todos={this.state.todos}
+              activeTags={this.state.activeTags}
+              toggleCompleted={this.handleToggleCompleted}
+              addItem={this.handleAddItem}
+              deleteItem={this.handleDeleteItem}
+            />
           </div>
           <div className="col-md-4">
-            <Sidebar />
+            <Sidebar
+              todos={this.state.todos}
+              activeTags={this.state.activeTags}
+              tagFilter={this.handleTagFilter}
+            />
           </div>
         </div>
       </div>
--- app/components/TodoList.js
+++ app/components/TodoList.js
@@ -1,9 +1,108 @@
 import React from 'react'
 
-export const TodoList = ({todos}) => {
-  return (
-    <div>
+export const TodoList = ({todos, activeTags, toggleCompleted, addItem, deleteItem}) => {
+  const todoItemsToIterate = []
 
+  if (!activeTags.length) {
+    todos.map((todo, i) => todoItemsToIterate.push(<TodoItem key={i} todo={todo} idx={i} toggleCompleted={toggleCompleted} deleteItem={deleteItem} />))
+  } else {
+    todos.map((todo, i) => {
+      todo.tags.map(tag => {
+        if (activeTags.indexOf(tag) !== -1) todoItemsToIterate.push(<TodoItem key={i} todo={todo} idx={i} activeTags={activeTags} toggleCompleted={toggleCompleted} deleteItem={deleteItem} />)
+      })
+    })
+  }
+
+  return (
+    <div className="panel panel-default">
+      <div className="panel-heading">
+        <h3 className="panel-title">
+          {activeTags.length ? `Filter by tag: ${activeTags.join(', ')}` : 'Todos:'}
+        </h3>
+      </div>
+      <div className="panel-body">
+        <div className="panel panel-default">
+          <AddItemPanel addItem={addItem} />
+        </div>
+        <div className="list-group">
+          {todoItemsToIterate}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const AddItemPanel = ({addItem}) => {
+  return (
+    <div className="panel-body">
+      <div className="form-horizontal">
+        <div className="form-group">
+          <label htmlFor="addItemField" className="col-xs-2">Title</label>
+          <div className="col-xs-10">
+            <input id="addItemField" type="text" className="form-control" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="addTagsField" className="col-xs-2">Tags</label>
+          <div className="col-xs-10">
+            <input id="addTagsField" type="text" className="form-control" />
+            <p className="help-block small">Tags should be separated with a comma and a space</p>
+          </div>
+        </div>
+      </div>
+      <button type="button" className="btn btn-success pull-right" onClick={() => addItem()}>Add Item</button>
+    </div>
+  )
+}
+
+const TodoItem = ({todo, idx, activeTags, toggleCompleted, deleteItem}) => {
+  const todoItem = todo.completed ? <del>{todo.title}</del> : todo.title
+  const todoTags = []
+
+  if (activeTags) {
+    activeTags.map(active => {
+      todo.tags.map((tag, i) => {
+        if (active === tag) {
+          todoTags.push(<TodoTag key={i} tag={tag} active/>)
+        } else {
+          todoTags.push(<TodoTag key={i} tag={tag} />)
+        }
+      })
+    })
+  } else {
+    todo.tags.map((tag, i) => {
+      todoTags.push(<TodoTag key={i} tag={tag} />)
+    })
+  }
+
+  return (
+    <div className={`list-group-item ${todo.completed ? 'list-group-item-warning' : ''}`}>
+      <span className="label label-danger pull-right" role="button" title="Delete this item" onClick={() => deleteItem(idx)}>
+        X
+      </span>
+      <div className="checkbox" style={{marginLeft: '8px'}}>
+        <label onClick={(e) => toggleCompleted(idx, e)} style={{cursor: 'pointer'}}>
+          <input type="checkbox" checked={todo.completed} style={{marginRight: '10px'}} />
+          {todoItem}
+        </label>
+      </div>
+      <div>
+        {todoTags}
+      </div>
+    </div>
+  )
+}
+
+export const TodoTag = ({tag, active, tagFilter}) => {
+  const clickable = {style: {display: 'inline-block', marginRight: '8px'}}
+  if (tagFilter) {
+    clickable.onClick = () => tagFilter(tag)
+    clickable.style.cursor = 'pointer'
+  }
+
+  return (
+    <div className={`label ${active ? 'label-primary' : 'label-default'}`} {...clickable}>
+      {tag}
     </div>
   )
 }
